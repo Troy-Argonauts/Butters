78ea8f0d2c9e3cc3a2b6c93dc485a4e97574c9ee
rohangajulaghosh <40278130+SolidityContract@users.noreply.github.com>
2023-01-21 14:12:14 -0500

    Update Constants
    Changed Constants

--- src/main/java/org/troyargonauts/Constants.java
+++ src/main/java/org/troyargonauts/Constants.java
@@ -2,7 +2,7 @@ package org.troyargonauts;
 
 public final class Constants {
 
-    public static final class PneumaticsConstants {
+    public interface Manipulator {
         public static final int kForwardChannel = 0;
         public static final int kReverseChannel = 1;
     }
