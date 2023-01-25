ee8a48fe3eb965c0ffa72725a94a43898183650c
TeoElRey <teodortopancs@gmail.com>
2023-01-24 19:22:44 -0500

    updated elevator
    Added periodic, PID command, values of limit switches and encoder
    
    Co-Authored-By: sgowda260 <116316950+sgowda260@users.noreply.github.com>

--- src/main/java/org/troyargonauts/subsystems/Elevator.java
+++ src/main/java/org/troyargonauts/subsystems/Elevator.java
@@ -1,24 +1,20 @@
 package org.troyargonauts.subsystems;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.SparkMaxLimitSwitch;
 import org.troyargonauts.Robot;
-
 public class Elevator extends SubsystemBase {
-    private CANSparkMax leftMotor;
-    private CANSparkMax rightMotor;
+    private final CANSparkMax leftMotor;
+    private final CANSparkMax rightMotor;
     private SparkMaxLimitSwitch upperLimitSwitch;
     private SparkMaxLimitSwitch lowerLimitSwitch;
-
+    private boolean upperLimitSwitchValue;
+    private boolean lowerLimitSwitchValue;
+    private Double leftMotorEncoder;
     PIDController pid;
-
     public Elevator() {
         leftMotor = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
         rightMotor = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -33,26 +29,32 @@ public class Elevator extends SubsystemBase {
 
         pid = new PIDController(1, 0 ,0, 0.2);
     }
+    @Override
+    public void periodic() {
+        leftMotorEncoder = leftMotor.getEncoder().getPosition();
+        upperLimitSwitchValue = getUpperLimitSwitchState();
+        lowerLimitSwitchValue = getLowerLimitSwitchState();
 
+    }
     public boolean getUpperLimitSwitchState() {
         return upperLimitSwitch.isLimitSwitchEnabled();
     }
     public boolean getLowerLimitSwitchState() {
         return lowerLimitSwitch.isLimitSwitchEnabled();
     }
-
     public void setElevatorPower(double speed) {
         leftMotor.set(speed);
         rightMotor.set(speed);
     }
-
     public void elevatorPID(double setpoint) {
         new PIDCommand(
                 pid,
-                () -> leftMotor.getEncoder().getPosition(),
+                () -> leftMotorEncoder,
                 setpoint,
                 this::setElevatorPower,
                 Robot.getElevator()
         );
     }
+
+
 }
