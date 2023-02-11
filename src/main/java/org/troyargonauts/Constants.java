b81b8fd27497e60d9ac39b14169763bccc631086
TeoElRey <teodortopancs@gmail.com>
2023-02-11 13:20:53 -0500

    Update Constants.java

--- src/main/java/org/troyargonauts/Constants.java
+++ src/main/java/org/troyargonauts/Constants.java
@@ -6,6 +6,12 @@ public final class Constants {
         public static final int kForwardChannel = 0;
         public static final int kReverseChannel = 1;
     }
+    public interface Limelight {
+        public static final double LIMELIGHT_HEIGHT = 32.625;
+        public static final double APRIL_TAG_HEIGHT = 18;
+        public static final double MOUNTING_ANGLE = 0;
+
+    }
 
 
 
