5b83ffaff5b441e50fd07501287de2d0bd4f2cfd
ASH-will-WIN <ashwinshrivastav4@gmail.com>
2023-02-23 13:03:25 -0500

    Removed getLimitSwitch methods
    -  Removed Both getLimitSwitch methods.
    - Fixed JavaDocs
    - Updated Smart Dashboard with limit switch states

--- src/main/java/org/troyargonauts/subsystems/Turret.java
+++ src/main/java/org/troyargonauts/subsystems/Turret.java
@@ -16,20 +16,17 @@ import org.troyargonauts.Robot;
 
 public class Turret extends SubsystemBase {
     private final CANSparkMax turretMotor;
-    public double turretPosition;
+    public double encoderPosition;
     public final SparkMaxLimitSwitch rightLimitSwitch;
     public final SparkMaxLimitSwitch leftLimitSwitch;
     private PIDController pid;
-
-    public boolean leftLimitSwitchIsActive;
-    public boolean rightLimitSwitchIsActive;
 
 
 
 
 
     /**
-     * Constructor for Turret Class. Instantiates motor, magnetic limit switches, PID Controllers, and encoders.
+     * Constructor for Turret Class. Instantiates motor, magnetic limit switches, PID Controller, and encoder.
      */
     public Turret() {
         turretMotor = new CANSparkMax(Constants.Turret.PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -42,10 +39,10 @@ public class Turret extends SubsystemBase {
     /**
      * This sets motor to given power. Will not allow turret to go past the two magnetic limit switches.
      * @param power Desired motor power.
-     * @param nerf Nerf of motor power.
-     */
+     * @param nerf Percentage nerf of motor power
+     **/
     public void setPower(double power, double nerf){
-//        if ((getLeftLimitSwitchState() && power < 0) || (getRightLimitSwitchState() && power > 0)) {
+//        if ((leftLimitSwitch.isLimitSwitchEnabled() && power < 0) || (rightLimitSwitch.isLimitSwitchEnabled() && power > 0)) {
 //            turretMotor.set(0);
 //        }
 //        else {
@@ -56,47 +53,33 @@ public class Turret extends SubsystemBase {
         turretMotor.set(power * nerf);
     }
 
+
+
     /**
      * Periodic will constantly check the encoder and limit switch values.
      * These values will later be used to make sure turrets reach positions and stay within limits.
      */
     @Override
     public void periodic(){
-        SmartDashboard.putNumber("Position", turretPosition);
+        SmartDashboard.putNumber("Position", encoderPosition);
         SmartDashboard.putNumber("Motor Rotations", turretMotor.getEncoder().getPosition());
         SmartDashboard.putNumber("Turret Rotations", (turretMotor.getEncoder().getPosition() / 125));
-        turretPosition = turretMotor.getEncoder().getPosition();
-    }
-
-    /**
-     * Gives status of left limit switch as a boolean value.
-     * @return Status of left limit switch.
-     */
-
-    public boolean getLeftLimitSwitchState(){
-        return leftLimitSwitch.isLimitSwitchEnabled();
-    }
-
-    /**
-     * Gives status of right limit switch as a boolean value.
-     * @return Status of right limit switch.
-     */
-
-    public boolean getRightLimitSwitchState() {
-        return rightLimitSwitch.isLimitSwitchEnabled();
+        SmartDashboard.putBoolean("Right Limit Switch", rightLimitSwitch.isLimitSwitchEnabled());
+        SmartDashboard.putBoolean("Right Limit Switch", leftLimitSwitch.isLimitSwitchEnabled());
+        encoderPosition = turretMotor.getEncoder().getPosition();
     }
 
 
     /**
-     * Using a PID command, turret will rotate to a setpoint using the PID Controller. 
+     * Turret will rotate to a setpoint using the PID Controller.
      * @param setpoint Setpoint for the Turret
      */
     public void turretPID(double setpoint){
         pid.setSetpoint(setpoint);
         new PIDCommand(
             pid,
-            () -> turretPosition,
-            () -> turretPosition,
+            () -> encoderPosition,
+            () -> encoderPosition,
             setpoint,
             output -> setPower(output, 0.5),
             Robot.getTurret()
