36c91f2beb4eab2ae7122631e7fc06f20fc9d4d5
rohangajulaghosh <40278130+SolidityContract@users.noreply.github.com>
2023-02-23 17:43:29 -0500

    Updated Arm JavaDocs

--- src/main/java/org/troyargonauts/Constants.java
+++ src/main/java/org/troyargonauts/Constants.java
@@ -2,20 +2,18 @@ package org.troyargonauts;
 
 public final class Constants {
 
-    public interface Manipulator {
-        public static final int kForwardChannel = 0;
-        public static final int kReverseChannel = 1;
-    }
-
     public interface Arm {
         public static final int ARM_MOTOR = 0;
         public static final int ARM_INTAKE = 1;
+
         public static final int TOP_ELBOW_ENCODER_LIMIT = 10;
         public static final int LOWER_ELBOW_ENCODER_LIMIT = 10;
         public static final int TOP_WRIST_ENCODER_LIMIT = 10;
         public static final int LOWER_WRIST_ENCODER_LIMIT = 10;
+
         public static final double FORWARD_INTAKE_SPEED = 0.25;
         public static final double REVERSE_INTAKE_SPEED = -0.25;
+        
         public static final double kP = 0;
         public static final double kI = 0;
         public static final double kD = 0;
--- src/main/java/org/troyargonauts/subsystems/Arm.java
+++ src/main/java/org/troyargonauts/subsystems/Arm.java
@@ -14,12 +14,12 @@ import org.troyargonauts.Robot;
  * @author Ashwin Shrivastav, Teodor Topan, Rohan Gajula-Ghosh
  */
 public class Arm extends SubsystemBase {
-    public final CANSparkMax elbowMotor;
-    public final CANSparkMax intakeMotor;
-    public final CANSparkMax wristMotor;
-    public final AbsoluteEncoder elbowEncoder;
-    public final AbsoluteEncoder wristEncoder;
-    PIDController pid;
+    private final CANSparkMax elbowMotor;
+    private final CANSparkMax intakeMotor;
+    private final CANSparkMax wristMotor;
+    private final AbsoluteEncoder elbowEncoder;
+    private final AbsoluteEncoder wristEncoder;
+    private final PIDController pid;
 
     /**
      * Here, the motors, absolute encoders, and PID Controller are instantiated.
@@ -40,7 +40,7 @@ public class Arm extends SubsystemBase {
     }
 
     /**
-     * Enums are the states of the intake rollers.
+     * Enums are the states of the intake rollers (FORWARD, OFF, BACKWARD).
      */
     public enum intakeState {
         FORWARD, OFF, BACKWARD
@@ -48,45 +48,25 @@ public class Arm extends SubsystemBase {
 
     /**
      * Intake roller state is set here.
-     *
-     * @param state desired state of intake rollers (FORWARD, OFF, BACKWARD) to a motor speed
+     * @param state desired state of intake rollers to a motor speed.
      */
     public void setIntakeState(intakeState state) {
         switch(state) {
             case FORWARD:
-                activateIntake();
+                intakeMotor.set(Constants.Arm.FORWARD_INTAKE_SPEED);
+                break;
             case OFF:
-                deactivateIntake();
+                intakeMotor.set(0);
+                break;
             case BACKWARD:
-                reverseIntake();
+                intakeMotor.set(Constants.Arm.REVERSE_INTAKE_SPEED);
+                break;
         }
     }
 
     /**
-     * Activates intake motors to FORWARD state
-     */
-    public void activateIntake() {
-        intakeMotor.set(Constants.Arm.FORWARD_INTAKE_SPEED);
-    }
-
-    /**
-     * Activates intake motors to OFF state
-     */
-    public void deactivateIntake() {
-        intakeMotor.set(0);
-    }
-
-    /**
-     * Activates intake motors to BACKWARD state
-     */
-    public void reverseIntake() {
-        intakeMotor.set(Constants.Arm.REVERSE_INTAKE_SPEED);
-    }
-
-    /**
-     * Sets Elbow to set speed given it is within encoder limits
-     *
-     * @param speed sets elbow motor to desired speed given that it is within the encoder limits
+     * Sets Elbow to set speed given it is within encoder limits.
+     * @param speed sets elbow motor to desired speed given that it is within the encoder limits.
      */
     public void setElbowPower(double speed) {
         if ((getElbowPosition() < Constants.Arm.TOP_ELBOW_ENCODER_LIMIT) || (getWristPosition() > Constants.Arm.LOWER_ELBOW_ENCODER_LIMIT)) {
@@ -98,9 +78,8 @@ public class Arm extends SubsystemBase {
     }
 
     /**
-     * Sets Wrist to set speed given it is within encoder limits
-     *
-     * @param speed sets wrist motor to desired speed given that it is within the encoder limits
+     * Sets Wrist to set speed given it is within encoder limits.
+     * @param speed sets wrist motor to desired speed given that it is within the encoder limits.
      */
     public void setWristPower(double speed) {
         if ((getWristPosition() < Constants.Arm.TOP_WRIST_ENCODER_LIMIT) || (getWristPosition() > Constants.Arm.LOWER_WRIST_ENCODER_LIMIT)) {
@@ -113,7 +92,6 @@ public class Arm extends SubsystemBase {
 
     /**
      * Returns elbow position
-     *
      * @return returns the position of elbow
      */
     public double getElbowPosition() {
@@ -122,7 +100,6 @@ public class Arm extends SubsystemBase {
 
     /**
      * Returns wrist position
-     *
      * @return returns the position of wrist
      */
     public double getWristPosition() {
@@ -130,32 +107,32 @@ public class Arm extends SubsystemBase {
     }
 
     /**
-     * Using a PID command, elbow will rotate to a setpoint using the PID Controller. Will mainly be used in autonomous.
-     * @param setpoint the point desired to be reached by the elbow
+     * Using a PID command, elbow will rotate to a setpoint using the PID Controller.
+     * @param setpoint the point desired to be reached by the elbow.
      */
     public PIDCommand elbowPid(double setpoint) {
         pid.setSetpoint(setpoint);
         return new PIDCommand(
-                pid,
-                this::getElbowPosition,
-                setpoint,
-                this::setElbowPower,
-                Robot.getArm()
+            pid,
+            this::getElbowPosition,
+            setpoint,
+            this::setElbowPower,
+            Robot.getArm()
         );
     }
 
     /**
-     * Using a PID command, wrist will rotate to a setpoint using the PID Controller. Will mainly be used in autonomous.
+     * Using a PID command, wrist will rotate to a setpoint using the PID Controller.
      * @param setpoint the point desired to be reached by the wrist
      */
     public PIDCommand wristPid(double setpoint) {
         pid.setSetpoint(setpoint);
         return new PIDCommand(
-                pid,
-                this::getWristPosition,
-                setpoint,
-                this::setWristPower,
-                Robot.getArm()
+            pid,
+            this::getWristPosition,
+            setpoint,
+            this::setWristPower,
+            Robot.getArm()
         );
     }
 }
