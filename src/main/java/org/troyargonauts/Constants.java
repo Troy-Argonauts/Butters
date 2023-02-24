24ae0d03a4dea80eff6399104fdc04c93d0ff0b6
SavageCabbage360 <abhinav.daram24@gmail.com>
2023-02-23 22:41:22 -0500

    reformatted code
    removed redundant code
    
    Co-Authored-By: Sharvayu Chavan <69215325+Sharvayu-Chavan@users.noreply.github.com>

--- src/main/java/org/troyargonauts/Constants.java
+++ src/main/java/org/troyargonauts/Constants.java
@@ -3,11 +3,11 @@ package org.troyargonauts;
 public final class Constants {
 
     public interface Intake{
-        public static final double SQUEEZE_MOTOR_SPEED = 0.2;
-        public static final double ROTATE_MOTOR_SPEED = 0.2;
+        double SQUEEZE_MOTOR_SPEED = 0.2;
+        double ROTATE_MOTOR_SPEED = 0.2;
 
-        public static final int SQUEEZE_MOTOR_PORT = 0;
-        public static final int ROTATE_MOTOR_PORT = 1;
+        int SQUEEZE_MOTOR_PORT = 0;
+        int ROTATE_MOTOR_PORT = 1;
 
     }
 
