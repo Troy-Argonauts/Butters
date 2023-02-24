169813f1aa55d2bc09f17b084c19a6ab711b1e39
SavageCabbage360 <abhinav.daram24@gmail.com>
2023-02-24 16:01:18 -0500

    Removed unnecessary code and added java docs
    Added java docs and removed manipulator pneumatics code as pneumatics are only going to be used on the elevator.

--- src/main/java/org/troyargonauts/Constants.java
+++ src/main/java/org/troyargonauts/Constants.java
@@ -2,10 +2,6 @@ package org.troyargonauts;
 
 public final class Constants {
 
-    public interface Manipulator {
-        public static final int kForwardChannel = 0;
-        public static final int kReverseChannel = 1;
-    }
     public interface Elevator {
         public static final int kElevatorForwardChannel = 0;
         public static final int kElevatorReverseChannel = 1;
--- src/main/java/org/troyargonauts/subsystems/Pneumatics.java
+++ src/main/java/org/troyargonauts/subsystems/Pneumatics.java
@@ -6,36 +6,40 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * Pneumatics are used on the elevator to lift and drop the elevator
+ * @author aarooshg, Solidarity Contract, firearcher2012, TheFlyingPig25, SavageCabbage360
+ */
 public class Pneumatics extends SubsystemBase {
-
-    private final DoubleSolenoid manipulatorSolenoid;
-    private DoubleSolenoid.Value manipulatorState;
+    /**
+     * Variables created for the solenoid controlling the elevator as well as the variable that specifies if the solenoid is in or out.
+     */
     private DoubleSolenoid elevatorSolenoid;
     private DoubleSolenoid.Value elevatorState;
 
+    /**
+     * Constructor for the pneumatics.
+     * The pneumatics are instantiated and are given values
+     */
+
     public Pneumatics() {
-        manipulatorSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Manipulator.kForwardChannel, Manipulator.kReverseChannel);
-        manipulatorState = DoubleSolenoid.Value.kForward;
+
         elevatorSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Elevator.kElevatorForwardChannel, Elevator.kElevatorReverseChannel);
         elevatorState = DoubleSolenoid.Value.kForward;
     }
 
+    /**
+     * This enum displays the states that the pneumatics can go in
+     * The states that the pneumatics solenoid can go in are FORWARD and REVERSE
+     */
     public enum State {
         FORWARD, REVERSE;
     }
 
-    public void setManipulatorSolenoid(State state){
-        switch (state) {
-            case REVERSE:
-                manipulatorState = DoubleSolenoid.Value.kReverse;
-                break;
-            case FORWARD:
-                manipulatorState = DoubleSolenoid.Value.kForward;
-                break;
-        }
-        manipulatorSolenoid.set(manipulatorState);
-    }
-
+    /**
+     *This switch-case is how the state of the pneumatics is changed
+     * @param state the state that pneumatic is going to be set to
+     */
     public void setElevatorSolenoid(State state) {
         switch (state) {
             case REVERSE:
