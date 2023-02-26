1d4d61a730c12af08397e707d84380539b695916
Sharvayu Chavan <69215325+Sharvayu-Chavan@users.noreply.github.com>
2023-02-25 20:25:43 -0500

    Intake Java Docs
    added intake java docs.
    
    Co-Authored-By: Abhinav Daram <76536884+SavageCabbage360@users.noreply.github.com>

--- src/main/java/org/troyargonauts/subsystems/Intake.java
+++ src/main/java/org/troyargonauts/subsystems/Intake.java
@@ -1,46 +1,77 @@
+/**
+ Represents the intake subsystem of the robot that includes the squeeze motor and rotate motor with their limit switches.
+ Provides methods for controlling the state of the squeeze motor and rotate motor.
+ */
 package org.troyargonauts.subsystems;
-
-
 import com.revrobotics.SparkMaxLimitSwitch;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.troyargonauts.Constants;
-
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
-
+/**
+ * Back Intake Code
+ * @author Sharvayu-Chavan SavageCabbage360
+ */
 public class Intake extends SubsystemBase {
-
+    /**
+     * The CANSparkMax object representing the squeeze motor.
+     */
     private static CANSparkMax squeezeMotor;
+
+    /**
+     * The CANSparkMax object representing the rotate motor.
+     */
     private static CANSparkMax rotateMotor;
+
+    /**
+     * The current state of the squeeze motor (either "OPEN", "CLOSE", or "STOP").
+     */
     private static String intakeSqueezeState;
+
+    /**
+     * The current state of the rotate motor (either "UP", "DOWN", or "STOP").
+     */
     private static String intakeRotateState;
 
+    /**
+     * The limit switch on the rotate motor in the forward direction.
+     */
     public static SparkMaxLimitSwitch rotateForwardLimitSwitch;
+
+    /**
+     * The limit switch on the rotate motor in the backward direction.
+     */
     public static SparkMaxLimitSwitch rotateBackwardLimitSwitch;
 
+    /**
+     * Constructs a new Intake object with the squeeze and rotate motors initialized to the ports specified in Constants.
+     * The limit switches are also initialized.
+     */
     public Intake() {
         squeezeMotor = new CANSparkMax(Constants.Intake.SQUEEZE_MOTOR_PORT, MotorType.kBrushless);
         rotateMotor = new CANSparkMax(Constants.Intake.ROTATE_MOTOR_PORT, MotorType.kBrushless);
         rotateForwardLimitSwitch = rotateMotor.getForwardLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyClosed);
         rotateBackwardLimitSwitch = rotateMotor.getReverseLimitSwitch(SparkMaxLimitSwitch.Type.kNormallyClosed);
-
     }
-
-
-   public enum squeezeStates {
+    /**
+     * Represents the possible states of the squeeze motor.
+     */
+    public enum squeezeStates {
         OPEN, CLOSE, STOP
     }
 
-   public enum rotateStates {
+    /**
+     * Represents the possible states of the rotate motor.
+     */
+    public enum rotateStates {
         UP, DOWN, STOP
     }
-
+    /**
+     * Sets the state of the squeeze motor based on the input squeezeStates enum.
+     * @param state The state to set the squeeze motor to (either "OPEN", "CLOSE", or "STOP").
+     */
     public static void setSqueezeIntakeState(squeezeStates state) {
         switch (state) {
             case OPEN:
@@ -58,6 +89,10 @@ public class Intake extends SubsystemBase {
         }
     }
 
+    /**
+     * Sets the state of the rotate motor based on the input rotateStates enum.
+     * @param state The state to set the rotate motor to (either "UP", "DOWN", or "STOP").
+     */
     public static void setRotateIntakeState(rotateStates state) {
         switch (state) {
             case UP:
@@ -86,7 +121,11 @@ public class Intake extends SubsystemBase {
     }
 
 
+    /**
 
+     This method is called periodically.
+     It updates the SmartDashboard with the current states of the intake squeeze, intake rotate, and the forward and backward limit switches of the intake rotate motor.
+     */
     @Override
     public void periodic() {
         SmartDashboard.putString("Intake Squeeze State", intakeSqueezeState);
