55488f90aaca88fbef09d07fbefb05a5c36f5077
Argonauts Driver Station <101407414+ArgonautsStation@users.noreply.github.com>
2023-03-06 20:49:07 -0500

    Added controller bindings
    Added controller bindings to test zach and dominic's intake
    
    Co-Authored-By: Abhinav Daram <76536884+SavageCabbage360@users.noreply.github.com>

--- src/main/java/org/troyargonauts/RobotContainer.java
+++ src/main/java/org/troyargonauts/RobotContainer.java
@@ -8,9 +8,14 @@ package org.troyargonauts;
 import org.troyargonauts.subsystems.Intake;
 import org.troyargonauts.subsystems.Intake.rotateStates;
 import org.troyargonauts.subsystems.Intake.squeezeStates;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+
+import java.awt.*;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -34,6 +39,20 @@ public class RobotContainer {
     /** Use this method to define your trigger->command mappings. */
     private void configureBindings()
     {
+        driver.getLBButton().onTrue(
+                        new InstantCommand(() -> Robot.getIntake().setRotateIntakeState(Intake.rotateStates.UP), Robot.getIntake()))
+                .onFalse(new InstantCommand(() -> Robot.getIntake().setRotateIntakeState(Intake.rotateStates.STOP), Robot.getIntake()));
+
+        driver.getRBButton().onTrue(
+                        new InstantCommand(() -> Robot.getIntake().setRotateIntakeState(Intake.rotateStates.DOWN), Robot.getIntake()))
+                .onFalse(new InstantCommand(() -> Robot.getIntake().setRotateIntakeState(Intake.rotateStates.STOP), Robot.getIntake()));
+
+        driver.getAButton().onTrue(
+                        new InstantCommand(() -> Robot.getIntake().setSqueezeIntakeState(Intake.squeezeStates.OPEN), Robot.getIntake()))
+                .onFalse(new InstantCommand(() -> Robot.getIntake().setSqueezeIntakeState(Intake.squeezeStates.STOP), Robot.getIntake()));
+        driver.getBButton().onTrue(
+                        new InstantCommand(() -> Robot.getIntake().setSqueezeIntakeState(Intake.squeezeStates.CLOSE), Robot.getIntake()))
+                .onFalse(new InstantCommand(() -> Robot.getIntake().setSqueezeIntakeState(Intake.squeezeStates.STOP), Robot.getIntake()));
         
     }
     
